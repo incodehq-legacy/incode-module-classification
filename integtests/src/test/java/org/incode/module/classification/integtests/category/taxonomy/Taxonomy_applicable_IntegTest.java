cf200b5f6f91f96dcd9f98d97dce25eb64b0c7cf
Jeroen van der Wal <jeroen@stromboli.it>
2016-08-04 14:46:08 +0200

    adds taxonomy applicable test

--- integtests/src/test/java/org/incode/module/classification/integtests/category/taxonomy/Taxonomy_applicable_IntegTest.java
+++ integtests/src/test/java/org/incode/module/classification/integtests/category/taxonomy/Taxonomy_applicable_IntegTest.java
@@ -16,16 +16,31 @@
  */
 package org.incode.module.classification.integtests.category.taxonomy;
 
-import org.incode.module.classification.dom.impl.applicability.ApplicabilityRepository;
-import org.incode.module.classification.dom.impl.category.CategoryRepository;
-import org.incode.module.classification.dom.impl.classification.ClassificationRepository;
-import org.incode.module.classification.dom.spi.ApplicationTenancyService;
-import org.incode.module.classification.fixture.dom.demo.first.DemoObjectMenu;
-import org.incode.module.classification.integtests.ClassificationModuleIntegTest;
-import org.junit.Before;
-import org.junit.Ignore;
+import java.util.List;
 
 import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import org.apache.isis.applib.services.wrapper.InvalidException;
+
+import org.incode.module.classification.dom.impl.applicability.Applicability;
+import org.incode.module.classification.dom.impl.applicability.ApplicabilityRepository;
+import org.incode.module.classification.dom.impl.category.CategoryRepository;
+import org.incode.module.classification.dom.impl.category.taxonomy.Taxonomy;
+import org.incode.module.classification.dom.impl.classification.ClassificationRepository;
+import org.incode.module.classification.dom.spi.ApplicationTenancyService;
+import org.incode.module.classification.fixture.dom.demo.first.DemoObject;
+import org.incode.module.classification.fixture.dom.demo.first.DemoObjectMenu;
+import org.incode.module.classification.fixture.dom.demo.other.OtherObject;
+import org.incode.module.classification.fixture.scripts.scenarios.ClassifiedDemoObjectsFixture;
+import org.incode.module.classification.fixture.scripts.teardown.ClassificationDemoAppTearDownFixture;
+import org.incode.module.classification.integtests.ClassificationModuleIntegTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Taxonomy_applicable_IntegTest extends ClassificationModuleIntegTest {
 
@@ -41,39 +56,60 @@ public class Taxonomy_applicable_IntegTest extends ClassificationModuleIntegTest
     @Inject
     ApplicationTenancyService applicationTenancyService;
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Before
     public void setUpData() throws Exception {
-        // fixtureScripts.runFixtureScript(new ClassificationDemoAppTearDownFixture(), null);
-        // fixtureScripts.runFixtureScript(new ClassifiedDemoObjectsFixture(), null);
+        fixtureScripts.runFixtureScript(new ClassificationDemoAppTearDownFixture(), null);
+        fixtureScripts.runFixtureScript(new ClassifiedDemoObjectsFixture(), null);
     }
 
-    @Ignore
-    public void happy_case() {
-
-        // eg set up for "/ITA", search for app tenancy "/"
-
-    }
-
-    @Ignore
+    @Test
     public void can_add_applicability_for_different_domain_types_with_same_atPath() {
+        // given
+        Taxonomy frenchColours = (Taxonomy) categoryRepository.findByParentAndName(null, "French Colours");
+        assertThat(applicabilityRepository.findByDomainTypeAndUnderAtPath(OtherObject.class, "/FRA")).isEmpty();
 
-        // eg given an applicability for "/ITA" and 'DemoObject', can also add an applicability for "/ITA" and 'OtherObject'
+        // when
+        wrap(frenchColours).applicable("/FRA", OtherObject.class.getName());
 
+        // then
+        assertThat(applicabilityRepository.findByDomainTypeAndUnderAtPath(OtherObject.class, "/FRA")).hasSize(1);
     }
 
-    @Ignore
+    @Test
     public void can_add_applicability_for_same_domain_types_with_different_atPath() {
 
         // eg given an applicability for "/ITA" and 'DemoObject', can also add an applicability for "/ITA/MIL" and 'DemoObject'
+        Taxonomy italianColours = (Taxonomy) categoryRepository.findByParentAndName(null, "Italian Colours");
+        final List<Applicability> byDomainTypeAndUnderAtPath = applicabilityRepository.findByDomainTypeAndUnderAtPath(DemoObject.class, "/ITA");
+        assertThat(byDomainTypeAndUnderAtPath).hasSize(2);
+        assertThat(byDomainTypeAndUnderAtPath).extracting(Applicability::getTaxonomy).extracting(Taxonomy::getFullyQualifiedName).containsOnly("Italian Colours", "Sizes");
 
+        // when
+        wrap(italianColours).applicable("/ITA/MIL", DemoObject.class.getName());
+
+        // then
+        final List<Applicability> byDomainTypeAndUnderAtPathNew = applicabilityRepository.findByDomainTypeAndUnderAtPath(DemoObject.class, "/ITA/MIL");
+        assertThat(byDomainTypeAndUnderAtPathNew).hasSize(3);
+        assertThat(byDomainTypeAndUnderAtPathNew).extracting(Applicability::getTaxonomy).extracting(Taxonomy::getFullyQualifiedName).containsOnly("Italian Colours", "Sizes");
     }
 
-    @Ignore
+    @Test
     public void cannot_add_applicability_if_already_has_applicability_for_given_domainType_and_atPath() {
 
         // eg set up for "/ITA" and 'DemoObject', cannot add again
+        // given
+        Taxonomy italianColours = (Taxonomy) categoryRepository.findByParentAndName(null, "Italian Colours");
+
+        // then
+        expectedException.expect(InvalidException.class);
+        expectedException.expectMessage("Already applicable for '/ITA' and '" + DemoObject.class.getName() + "'");
+
+        // when
+        wrap(italianColours).applicable("/ITA", DemoObject.class.getName());
 
     }
-
 
 }
